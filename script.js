b99ca5b0756efc839b4f1d17402e2179b1728e39
Ali Jan <alijandeveloper15@gmail.com>
2025-03-10 11:48:49 -0700

    solid col scri

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ const saveBtn = document.getElementById("save-btn");
 const colorCode = document.getElementById("color-code");
 const favorites = document.getElementById("favorites");
 let autoChangeInterval;
+
+function getRandomHexColor() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function applySolidColor() {
+    const color = getRandomHexColor();
+    document.body.style.background = color;
+    colorCode.textContent = color;
+}
