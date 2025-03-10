65b6df6225197f135745669cbfd6203935d1843d
Ali Jan <alijandeveloper15@gmail.com>
2025-03-10 11:48:10 -0700

    html updated and get elements

---
+++ script.js
@@ -0,0 +1,10 @@
+/* script.js */
+const solidBtn = document.getElementById("solid-btn");
+const gradientBtn = document.getElementById("gradient-btn");
+const imageBtn = document.getElementById("image-btn");
+const autoBtn = document.getElementById("auto-btn");
+const copyBtn = document.getElementById("copy-btn");
+const saveBtn = document.getElementById("save-btn");
+const colorCode = document.getElementById("color-code");
+const favorites = document.getElementById("favorites");
+let autoChangeInterval;
